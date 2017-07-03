c3f334f61d48d3200c881798b1c8d204d8761517
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-03 22:30:24 +0200

    adding default presale

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,18 @@
+var SafeMath = artifacts.require("./math/SafeMath.sol")
+var Presale = artifacts.require("./Presale.sol");
+
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(SafeMath);
+  deployer.link(Ownable, Presale);
+  if (network != "live" && network != "ropsten") {
+    deployer.deploy(Presale, 
+    0, 
+    250,
+    )
+    deployer.deploy(FundRequestPreSale,
+      "0",
+      "250",
+      "4",
+      "0xe5a0aca19ca6326aa52ea8cdc795602d0675d5f1f106788e8dddfbdf7cf548d0");
+  }
+};
