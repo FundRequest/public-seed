5d22bba51ae394fcde5c769c88f765d42c686807
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-16 16:59:09 +0200

    adding other substructure

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var del = require('del');
+
+var paths = {
+  images: ['app/images/**.png', 'app/favicons/favicon.ico'],
+  scripts: ['app/**/**.js'],
+  css: ['app/**/**.css'],
+  pages: ['app/**/**.html']
+};
+
+// Not all tasks need to use streams
+// A gulpfile is just another node program and you can use any package available on npm
+gulp.task('clean', function () {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del(['dist']);
+});
+
+gulp.task('images', function () {
+  return gulp.src(paths.images)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(paths.fonts)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function () {
+  return gulp.src(paths.css)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src(paths.scripts)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('pages', function () {
+  return gulp.src(paths.pages)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist'));
+});
+
+
+// Rerun the task when a file changes
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.pages, ['pages']);
+  gulp.watch(paths.css, ['css']);
+  gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'pages', 'css', 'fonts', 'scripts', 'images']);
+
+gulp.task('build', ['pages', 'css', 'fonts', 'scripts', 'images']);
