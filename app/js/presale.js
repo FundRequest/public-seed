8490ce7ebb26fec9912baa7adfe7fc58ac3661df
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-25 10:15:17 +0200

    new version

--- app/js/presale.js
+++ app/js/presale.js
@@ -1,16 +1,51 @@
-var Presale;
-
 window.App = {
-  loadContract: function () {
+  ex: {
+    accounts: [],
+    Presale: null,
+    selectedAccount: null
+  },
+  loadContract: function (_callback) {
     $.getJSON("./contracts/Presale.json", function (Presale_json) {
       console.log(Presale_json);
-      Presale = TruffleContract(Presale_json);
-      Presale.setProvider(window.web3.currentProvider);
+      App.ex.Presale = TruffleContract(Presale_json);
+      App.ex.Presale.setProvider(window.web3.currentProvider);
+      _callback();
     });
+  },
+  init: function () {
+    $("#btnBuy").click(App.buy);
+    $("#btnProxyBuy").click(function () {
+
+    });
+    this.loadContract(this.start);
+  },
+  allow: function () {
+    App.ex.Presale.deployed().then(function (instance) {
+      return instance.allow(instance.address, {
+        from: web3.eth.accounts[0]
+      });
+    }).then(function (result) {
+      console.log(result);
+    });
+  },
+  buy: function () {
+    App.ex.Presale.deployed().then(function (instance) {
+      return instance.buyTokens(instance.address, {
+        from: web3.eth.accounts[0],
+        value: web3.toWei(1)
+      });
+    }).then(function (result) {
+      console.log(result);
+    });
+  },
+  onBuy: function (result) {
+    console.log(result);
+  },
+  requestProxyBuy: function () {
+
   },
   start: function () {
     var self = this;
-    self.loadContract();
     web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
         alert("There was an error fetching your accounts.");
@@ -20,28 +55,28 @@ window.App = {
         alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
         return;
       }
-      accounts = accs;
+      App.ex.accounts = accs;
 
       var x = document.getElementById("accountSelect");
 
-      var l = accounts.length;
+      var l = App.ex.accounts.length;
       for (i = 0; i < l; i++) {
         var option = document.createElement("option");
-        option.text = accounts[i];
-        console.log(accounts[i]);
+        option.text = App.ex.accounts[i];
+        console.log(App.ex.accounts[i]);
         x.add(option);
       }
       $('#accountSelect').material_select();
-      $("#accountSelect").change(function(e){
-        var selectedAccount = ($( "#accountSelect option:selected" ).first().text());
+      $("#accountSelect").change(function (e) {
+        App.ex.selectedAccount = ($("#accountSelect option:selected").first().text());
       });
-      self.refreshContractInformation();
+      App.refreshContractInformation();
     });
   },
   refreshContractInformation: function () {
     var self = this;
     var presale;
-    Presale.deployed().then(function (instance) {
+    App.ex.Presale.deployed().then(function (instance) {
       presale = instance;
       return presale.rate.call();
     }).then(function (_rate) {
@@ -52,21 +87,23 @@ window.App = {
       return presale.investorCount.call();
     }).then(function (_investorCount) {
       $("#fndTotalBackers").html(_investorCount.toNumber());
+      return presale.owner.call();
+    }).then(function (_owner) {
+      console.log(_owner);
     });
-  },
+  }
 };
 
-
-window.addEventListener('load', function () {
+$(document).ready(function () {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.log("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
-  App.start();
+  App.init();
 });
