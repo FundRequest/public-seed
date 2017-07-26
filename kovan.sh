a03b5b0a29648e9d34e1479677be86020d677fd5
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-26 15:37:55 +0200

    added kovan config

---
+++ kovan.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl http://github-faucet.kovan.network/url --data "address=https://gist.github.com/Qkyrie/7a944bacd0955336ddfdf68eec45fb68"
+curl http://github-faucet.kovan.network/url --data "address=https://gist.github.com/Qkyrie/962c1f9b083dba78efc8ac18124451c2"
+curl http://github-faucet.kovan.network/url --data "address=https://gist.github.com/Qkyrie/58582327c1dd8f077fad695753759f8b"
