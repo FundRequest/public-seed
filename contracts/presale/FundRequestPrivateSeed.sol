739f6da50a4b4c479703c646cf7f790c357521a6
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-27 15:41:28 +0200

    mistakes were made

--- contracts/presale/FundRequestPrivateSeed.sol
+++ contracts/presale/FundRequestPrivateSeed.sol
@@ -27,7 +27,7 @@ contract FundRequestPrivateSeed is Pausable {
    */ 
   event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint value, uint amount);
   
-  function Presale(uint _rate, address _wallet) {
+  function FundRequestPrivateSeed(uint _rate, address _wallet) {
     require(_rate > 0);
     require(_wallet != 0x0);
 
