bea8b100163086831577f6afc1d82794c8634037
davyvanroy <davy.van.roy@gmail.com>
2017-08-26 23:30:26 +0200

    update libs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 var FundRequestPrivateSeed = artifacts.require("./presale/FundRequestPrivateSeed.sol");
-var SafeMathLib = artifacts.require("./math/SafeMath.sol");
+var SafeMath = artifacts.require("./math/SafeMath.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(SafeMathLib);
-  deployer.link(SafeMathLib, FundRequestPrivateSeed);
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, FundRequestPrivateSeed);
   deployer.deploy(FundRequestPrivateSeed,
     3575,
     accounts[0]
