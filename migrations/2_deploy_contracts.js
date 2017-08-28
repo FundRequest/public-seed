57959e505450d19f04a04da44e5d56bd46ccc769
davyvanroy <davy.van.roy@gmail.com>
2017-08-28 23:11:23 +0200

    update default FND/ETH rate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = function(deployer, network, accounts) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, FundRequestPrivateSeed);
   deployer.deploy(FundRequestPrivateSeed,
-    3575,
+    4500,
     accounts[0]
   );
 };
