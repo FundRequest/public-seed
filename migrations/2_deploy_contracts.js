bf661bcf67b972894ebc5f3883b3613216bdf11d
Karel Striegel <karel.striegel@ipc.be>
2017-09-08 18:28:15 +0200

    Rename class to public

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-var FundRequestPrivateSeed = artifacts.require("FundRequestPrivateSeed");
+var FundRequestPublicSeed = artifacts.require("FundRequestPublicSeed");
 var SafeMath = artifacts.require("SafeMath");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(SafeMath);
-  deployer.link(SafeMath, FundRequestPrivateSeed);
-  deployer.deploy(FundRequestPrivateSeed,
-    4500,
+  deployer.link(SafeMath, FundRequestPublicSeed);
+  deployer.deploy(FundRequestPublicSeed,
+    3600,
     accounts[0]
   );
 };
