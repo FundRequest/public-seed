526d86bcd6cdb4ee5723e2bc85e302b3a84ea9f7
Karel Striegel <karel.striegel@gmail.com>
2017-09-08 18:25:54 +0200

    Rename contract for public seed

---
+++ contracts/presale/FundRequestPublicSeed.sol
@@ -0,0 +1,95 @@
+pragma solidity ^0.4.15;
+
+import '../math/SafeMath.sol';
+import '../zeppelin/Pausable.sol';
+
+contract FundRequestPrivateSeed is Pausable {
+  using SafeMath for uint;
+
+  // address where funds are collected
+  address public wallet;
+  // how many token units a buyer gets per wei
+  uint public rate;
+  // amount of raised money in wei
+  uint public weiRaised;
+
+  mapping(address => uint) public deposits;
+  mapping(address => uint) public balances;
+  address[] public investors;
+  uint public investorCount;
+  mapping(address => bool) public allowed;
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint value, uint amount);
+
+  function FundRequestPrivateSeed(uint _rate, address _wallet) {
+    require(_rate > 0);
+    require(_wallet != 0x0);
+
+    rate = _rate;
+    wallet = _wallet;
+  }
+  // low level token purchase function
+  function buyTokens(address beneficiary) payable whenNotPaused {
+    require(validBeneficiary(beneficiary));
+    require(validPurchase());
+    require(validPurchaseSize());
+    bool existing = deposits[beneficiary] > 0;
+    uint weiAmount = msg.value;
+    uint updatedWeiRaised = weiRaised.add(weiAmount);
+    // calculate token amount to be created
+    uint tokens = weiAmount.mul(rate);
+    weiRaised = updatedWeiRaised;
+    deposits[beneficiary] = deposits[beneficiary].add(msg.value);
+    balances[beneficiary] = balances[beneficiary].add(tokens);
+    if(!existing) {
+      investors.push(beneficiary);
+      investorCount++;
+    }
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+    forwardFunds();
+  }
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+  function validBeneficiary(address beneficiary) internal constant returns (bool) {
+      return allowed[beneficiary] == true;
+  }
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal constant returns (bool) {
+    return msg.value != 0;
+  }
+  // @return true if the amount is higher then 25ETH
+  function validPurchaseSize() internal constant returns (bool) {
+    return msg.value >=25000000000000000000;
+  }
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances[_owner];
+  }
+  function depositsOf(address _owner) constant returns (uint deposit) {
+    return deposits[_owner];
+  }
+  function allow(address beneficiary) onlyOwner {
+    allowed[beneficiary] = true;
+  }
+  function updateRate(uint _rate) onlyOwner whenPaused {
+    rate = _rate;
+  }
+
+  function updateWallet(address _wallet) onlyOwner whenPaused {
+    require(_wallet != 0x0);
+    wallet = _wallet;
+  }
+
+  // fallback function can be used to buy tokens
+  function () payable {
+    buyTokens(msg.sender);
+  }
+}
