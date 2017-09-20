6227cb81f5aa75b0843af624894ae590c0c0c2c5
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-20 21:12:45 +0200

    check for deposits instead of plain msg.value

--- test/PurchaseSizeFunctionality.js
+++ test/PurchaseSizeFunctionality.js
@@ -52,6 +52,32 @@ contract('FundRequestPublicSeed', function (accounts) {
         }
     });
 
+    it('should not be possible to invest with amount more than 20 ether in whitelist period in multiple tries', async function () {
+        await seed.allow(accounts[1], {
+            from: accounts[0]
+        });
+        await seed.buyTokens(accounts[1], {
+            from: accounts[0],
+            value: web3.toWei(10)
+        });
+        await seed.buyTokens(accounts[1], {
+            from: accounts[0],
+            value: web3.toWei(9)
+        });
+        try {
+            await seed.buyTokens(accounts[1], {
+                from: accounts[0],
+                value: web3.toWei(2)
+            });
+            expect.fail('we were able to buy tokens');
+        } catch (error) {
+            assert(
+                error.message.indexOf('invalid opcode') >= 0,
+                'releaseTokenTransfer should throw an opCode exception.'
+            );
+        }
+    });
+
     it('should be possible to invest with amount more than 20 ether in a nonwhitelist period', async function () {
         await seed.allowEveryone({
             from: accounts[0]
