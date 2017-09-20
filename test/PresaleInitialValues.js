8820bbdf1e649ad9062953923292973e470b499c
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-20 20:47:19 +0200

    adding more tests

--- test/PresaleInitialValues.js
+++ test/PresaleInitialValues.js
@@ -1,27 +1,39 @@
-var Presale = artifacts.require("FundRequestPrivateSeed");
+const PUB_SEED = artifacts.require('./presale/FundRequestPublicSeed.sol');
+const expect = require('chai').expect;
 
-contract('FundRequestPrivateSeed', function (accounts) {
-  it("it should have the correct rate", function () {
-    return Presale.deployed().then(function (instance) {
-      return instance.rate.call();
-    }).then(function (_rate) {
-      assert.equal(_rate.valueOf(), 4500, "Rate was not correct");
-    });
+
+contract('FundrequestPublicSeed', function (accounts) {
+
+  let seed;
+  const owner = accounts[0];
+
+  beforeEach(async function () {
+    seed = await PUB_SEED.new(3600, 1000, owner);
   });
 
-   it("it should have 0 backers to start", function () {
-    return Presale.deployed().then(function (instance) {
-      return instance.investorCount.call();
-    }).then(function (_ic) {
-      assert.equal(_ic.valueOf(), 0, "investorcount was not correct");
-    });
+  it("it should have the correct rate", async function () {
+    let initialRate = (await seed.rate.call()).toNumber();
+    expect(initialRate).to.equal(3600);
   });
 
-   it("it should have 0 wei to start", function () {
-    return Presale.deployed().then(function (instance) {
-      return instance.weiRaised.call();
-    }).then(function (weiRaised) {
-      assert.equal(weiRaised.valueOf(), 0, "weiRaised was not correct");
-    });
+  it("it should have 0 backers to start", async function () {
+    let ic = (await seed.investorCount.call()).toNumber();
+    expect(ic).to.equal(0);
   });
+
+  it("it should have a max wei cap as defined in eth", async function () {
+    let cap = web3.fromWei((await seed.weiMaxCap.call()).toNumber());
+    expect(cap.toString()).to.equal(1000);
+  });
+
+  it("it should have a rate as defined", async function () {
+    let cap = (await seed.rate.call()).toNumber();
+    expect(cap).to.equal(3600);
+  });
+
+  it("it should have a wallet as defined", async function () {
+    let _wallet = await seed.wallet.call();
+    expect(_wallet).to.equal(accounts[0]);
+  });
+
 });
