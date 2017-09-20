9286055d16fba03c1e079d69ddee03588b365205
Quinten De Swaef <quintendeswaef@gmail.com>
2017-09-20 20:47:19 +0200

    adding more tests

---
+++ test/PurchaseSizeFunctionality.js
@@ -0,0 +1,66 @@
+const PUB_SEED = artifacts.require('./presale/FundRequestPublicSeed.sol');
+const expect = require('chai').expect;
+
+contract('FundRequestPublicSeed', function (accounts) {
+
+    let seed;
+    const owner = accounts[0];
+
+    beforeEach(async function () {
+        seed = await PUB_SEED.new(3600, 1000, owner);
+    });
+
+    it('should be possible to invest with amount less than 20 ether', async function () {
+        await seed.allowEveryone({
+            from: accounts[0]
+        });
+        await seed.buyTokens(accounts[1], {
+            from: accounts[0],
+            value: web3.toWei(19)
+        });
+        let weiRaised = (await seed.weiRaised.call()).toNumber();
+        expect(weiRaised.toString()).to.equal(web3.toWei(19));
+    });
+
+    it('should be possible to invest with amount less than 20 ether in a whitelist period', async function () {
+        await seed.allow(accounts[1], {
+            from: accounts[0]
+        });
+        await seed.buyTokens(accounts[1], {
+            from: accounts[0],
+            value: web3.toWei(19)
+        });
+        let weiRaised = (await seed.weiRaised.call()).toNumber();
+        expect(weiRaised.toString()).to.equal(web3.toWei(19));
+    });
+
+    it('should not be possible to invest with amount more than 20 ether in whitelist period', async function () {
+        await seed.allow(accounts[1], {
+            from: accounts[0]
+        });
+        try {
+            await seed.buyTokens(accounts[1], {
+                from: accounts[0],
+                value: web3.toWei(21)
+            });
+            expect.fail('we were able to buy tokens');
+        } catch (error) {
+            assert(
+                error.message.indexOf('invalid opcode') >= 0,
+                'releaseTokenTransfer should throw an opCode exception.'
+            );
+        }
+    });
+
+    it('should be possible to invest with amount more than 20 ether in a nonwhitelist period', async function () {
+        await seed.allowEveryone({
+            from: accounts[0]
+        });
+        await seed.buyTokens(accounts[1], {
+            from: accounts[0],
+            value: web3.toWei(21)
+        });
+        let weiRaised = (await seed.weiRaised.call()).toNumber();
+        expect(weiRaised.toString()).to.equal(web3.toWei(21));
+    });
+});
